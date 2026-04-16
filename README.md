c202ba3fa00727f2b7791d82e1b6b7adecc79d83
Jakub Gonet <jakub.gonet@swmansion.com>
2026-04-16 13:16:51 +0200

    Add package metadata

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Drum
 
-**TODO: Add description**
+Drum is an OTP application for running shell pipelines.
 
 ## Installation
 
@@ -18,4 +18,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/drum>.
-
--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule Drum.MixProject do
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      description: "An OTP application for running shell pipelines.",
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -28,11 +31,30 @@ defmodule Drum.MixProject do
     [
       {:erlexec, "~> 2.2"},
       {:owl, "~> 0.13"},
-      {:dotenvy, "~> 0.8", optional: true},
+      {:dotenvy, "~> 0.8"},
       {:file_system, "~> 1.1"},
-      {:glob_ex, "~> 0.1.11"}
+      {:glob_ex, "~> 0.1.11"},
+      {:ex_doc, "~> 0.40.1", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/jgonet/drum"
+      },
+      files: ~w(lib mix.exs README.md LICENSE)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_url: "https://github.com/jgonet/drum"
     ]
   end
 end
